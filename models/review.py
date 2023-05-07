ca86cc1cb57644fd286b2ed749e25888f312a2fe
sagebeme <eudicas5k@gmail.com>
2023-05-07 13:08:42 +0300

    Model updates

--- models/review.py
+++ models/review.py
@@ -1,17 +1,24 @@
-#!/usr/bin/python3
-""" Review module for the HBNB project """
+#!/usr/bin/python
+""" holds class Review"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.sql.schema import ForeignKey
 from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
-    if storage_type == 'db':
-        __tablename__ = "reviews"
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
